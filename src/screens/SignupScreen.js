b1876e071f3d4ac6b39aea3ac033264f9f930a1c
Joon <joonseo.an@outlook.com>
2020-02-13 23:58:47 -0500

    added gradient

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,23 +1,80 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-// import styled from 'styled-components';
-// import {widthPercentageToDP as wp, heightPercentageToDP as hp} 
-//  from 'react-native-responsive-screen';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import styled from 'styled-components';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } 
+ from 'react-native-responsive-screen';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const SignupScreen = ({ navigation }) => {
     return(
-        <View>
- 
-            <Button 
-                title="Go to Signin"
-                onPress={ () => navigation.navigate('Signin')}
-            />
-            <Button 
-                title="Go to main flow"
-                onPress={ () => navigation.navigate('mainFlow') }
-            />
-        </View>
-    )
+        <SignupContainer>
+            <LinearGradient 
+                style={ [styles.signupGroup, {
+                    shadowColor: "#000000",
+        shadowOffset: {
+            width: 5,
+            height: 7,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 15,
+        elevation: 23
+                }] }
+                colors={ [ 'rgb(245, 255, 250)', '#F0F8FF', '#00FFFF' ]}
+                start={[0.5, 0.8 ]}
+                end={[0, 0.1 ]}
+            >              
+                <Text>Signup</Text>            
+            </LinearGradient>
+        </SignupContainer>
+    );
 }
+
+const SignupContainer = styled.View`
+    background-color: rgba(255, 255, 0, 0.05);    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+`;
+
+const styles = StyleSheet.create({
+    signupGroup: {
+        width: 350,
+        height: 600,
+        // backgroundColor: 'rgb(245, 255, 250)',
+        borderRadius: 15,
+        // shadowColor: "#000000",
+        // shadowOffset: {
+        //     width: 5,
+        //     height: 7,
+        // },
+        // shadowOpacity: 1,
+        // shadowRadius: 15,
+        // elevation: 17
+    }
+});
+
+// const SignupGroup = styled.LinearGradient`
+//     width: ${ wp('90%') };
+//     height: ${ hp('70%') };
+//     background-color: rgb(245, 255, 250);
+//     border-radius: 30px;
+// `;
+
+// const signupShadow = {
+//     shadowColor: "#000000",
+//     shadowOffset: {
+//         width: 5,
+//         height: 7,
+//     },
+//     shadowOpacity: 1,
+//     shadowRadius: 15,
+//     elevation: 17,
+//     width: `${ wp('90%') }`,
+//     height: `${ hp('70%') }`,
+//     // backgroundColor: 'rgb(245, 255, 250)',
+//     borderRadius: 30,
+// }
 
 export default SignupScreen;
